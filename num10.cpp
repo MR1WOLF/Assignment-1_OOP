b2974fa8d0ef4b26956533965bb3674a1e8c7570
Noorpr <100091221+Noorpr@users.noreply.github.com>
2022-10-08 21:09:41 +0200

    number 10 in task 2

---
+++ num10.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+string phishing_list[] = {"label", "invoice", "post", "document", "postal", "calculations", "copy", "fedex", "statment", "financial", "dhl", "usps", "8", "notification", "amazon", "personal", "warning", "paypal", "visa", "bank", "security", "offical", "urgent", "alert", "citibank", "atm", "confirm", "address", "telephone", "ebay"};
+int phishing_points[] = {3, 3, 3, 3, 2, 2, 2, 2, 2, 1, 2, 3, 1, 2, 3, 3, 2, 1, 1, 2, 3, 3, 2, 2, 1, 1, 1, 2, 3, 1};
+vector<string> result;
+vector<string> showup;
+vector<int> occurence;
+
+int main()
+{
+    string word;
+    ifstream input;
+    input.open("Hello.txt");
+
+    while (!input.eof())
+    {
+        input >> word;
+        result.push_back(word);
+    }
+    for (int i = 0; i < 30; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < result.size(); j++)
+        {
+            if (phishing_list[i] == result[j])
+            {
+                count++;
+            }
+        }
+        occurence.push_back(count);
+    }
+
+    cout << "word"
+         << "    "
+         << "#"
+         << "    "
+         << "points" << endl;
+
+    for (int i = 0; i < occurence.size(); i++)
+    {
+        if (occurence[i] != 0)
+        {
+            cout << phishing_list[i] << "    " << occurence[i] << "    " << occurence[i] * phishing_points[i] << endl;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < occurence.size(); i++)
+    {
+        if (occurence[i] != 0)
+        {
+            sum += (occurence[i] * phishing_points[i]);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    cout << "The total points are : " << sum << endl;
+
+    input.close();
+    cout << "done!";
+    return 0;
+}
