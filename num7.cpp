e358a38c90331004dc11d7af734eceb84719846d
Noorpr <100091221+Noorpr@users.noreply.github.com>
2022-10-18 11:28:03 +0200

    number 7 in task 2

---
+++ num7.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+bool bear(int n){
+    if (n == 42){
+        return true;
+    }
+    else if (n < 42){
+        return false;
+    }
+    else{
+        if (n % 5 == 0){
+            return bear(n - 42);
+        }
+        if (n % 3 == 0){
+            int f_digit = n % 10;
+            int s_digit = ((n % 100) / 10);
+            return bear(n - (f_digit * s_digit));
+        }
+        if (n % 2 == 0){
+            if (n % 4 == 0 && n / 2 < 42){
+                int f_digit = n % 10;
+                int s_digit = ((n % 100) / 10);
+                return bear(n - (f_digit * s_digit));
+            }
+            else{
+                return bear(n / 2);
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+    int x = 1042;
+    cout << bear(x) << endl;
+    return 0;
+}
