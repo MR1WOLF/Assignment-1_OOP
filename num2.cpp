6dcc6b5f1e8d243c5f1d04875738f86415a56abf
Noorpr <100091221+Noorpr@users.noreply.github.com>
2022-10-25 10:32:14 +0200

    Update num2.cpp

--- num2.cpp
+++ num2.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 using namespace std;
-
+// Function overloaded to print the vector
 ostream &operator<<(ostream &os, const vector<string> list)
 {
     for (int i = 0; i < list.size(); i++)
@@ -21,16 +21,16 @@ int main()
     cout << split("nooooooor,jamal,f,1322", ",");
     return 0;
 }
-
+// Function to split the string
 vector<string> split(string target, string delimiter)
 {
     vector<string> list;
-    target.append(delimiter);
+    target.append(delimiter); // Append the delimiter to the end of the string
     while (target.find(delimiter) && !target.empty())
     {
-        int index = target.find(delimiter);
-        list.push_back(target.substr(0, index));
-        target.erase(0, index + 1);
+        int index = target.find(delimiter); // Find the index of the delimiter
+        list.push_back(target.substr(0, index)); // Push the substring to the vector but not included the delimiter
+        target.erase(0, index + 1);// Erase the substring from the string
     }
     return list;
 }
